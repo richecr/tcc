ce98aecdef5ee6290252a199eb65f34bedecbc84
Rich Ramalho <richelton14@gmail.com>
2022-02-15 23:18:15 -0300

    get drawings of pdf

--- extract_text/extract_pdfminer_six.py
+++ extract_text/extract_pdfminer_six.py
@@ -1,11 +1,28 @@
+from ctypes import cast
 from io import StringIO
+from typing import Dict, Iterable, Iterator, List, Optional, Tuple
 
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
+from pdfminer.utils import apply_matrix_pt, Matrix, Rect, fsplit, Plane, uniq
+from pdfminer.converter import PDFConverter, TextConverter
+from pdfminer.layout import (
+    LAParams,
+    LTPage,
+    LTTextBox,
+    LTChar,
+    LTFigure,
+    LTTextLine,
+    LTTextBoxVertical,
+    LTLayoutContainer,
+    LTTextBoxHorizontal,
+    LTTextLineHorizontal,
+    LTTextLineVertical,
+    LTComponent,
+    IndexAssigner
+)
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfparser import PDFParser, PDFStreamParser
 
 from utils import write_txt_pb
 
@@ -17,7 +34,8 @@ def extract_pdfminer_six(pdf, uf):
         doc = PDFDocument(parser)
         rsrcmgr = PDFResourceManager()
         device = TextConverter(
-            rsrcmgr, output_string, codec="utf-8", laparams=LAParams())
+            rsrcmgr, output_string, codec="utf-8",
+            laparams=LAParams())
         interpreter = PDFPageInterpreter(rsrcmgr, device)
         for page in PDFPage.create_pages(doc):
             interpreter.process_page(page)
@@ -26,7 +44,13 @@ def extract_pdfminer_six(pdf, uf):
     device.close()
     output_string.close()
     content = content.replace("�", '.')
-    filename = 'output_{}.txt'.format(pdf.split("/")[3])
-    path_dir = "./outputs/pdfminer_six_dou_{}/".format(uf)
+    filename = 'output_{}.txt'.format("teste")
+    path_dir = "./outputs/pdfminer_six_dou_{}_2/".format(uf)
+    print("AQUI")
+    c = ""
+    for a in content.split("ESTADO DO ACRE\n"):
+        c += a
+        c += "\n\n++++++++++++++++++++++++++++++++++++++\n\n"
+
     write_txt_pb(
-        content, filename=filename, dir=path_dir)
+        c, filename=filename, dir=path_dir)
--- extract_text/pymupdf.py
+++ extract_text/pymupdf.py
@@ -8,14 +8,20 @@ pdf = fitz.open('./pdfs_samples/ac/dou_ac_17_12_21.pdf')
 count = 1
 pages = []
 for page in pdf:
-    if count == 9:
-        dict_page = page.get_text("dict", flags=24)
-        text_page = json.dumps(dict_page)
-        text_page = text_page.replace("�", '.')
-        dict_page = json.loads(text_page)
-        pages.append(dict_page)
-        break
-    else:
-        count += 1
+    paths = page.get_cdrawings()
+    rects_interested = []
+    for path in paths:
+        if path['type'] == 's' and len(path['items']) == 1:
+            if path['color'] == (0.13669031858444214, 0.12195010483264923, 0.1252918243408203):
+                page.draw_rect(path["rect"], color=(1, 0, 0))
+                rects_interested.append(path)
+
+    dict_page = page.get_text("dict", flags=24)
+    text_page = json.dumps(dict_page)
+    text_page = text_page.replace("�", '.')
+    dict_page = json.loads(text_page)
+    pages.append(dict_page)
+
+pdf.ez_save("x.pdf")
 
 write_json(pages, filename="data_pymupdf.json")
