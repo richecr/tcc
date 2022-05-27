954a7e94cbf08d3b004edb3c36402002a6a44ee3
Rich Ramalho <richelton14@gmail.com>
2022-05-26 22:54:28 -0300

    converter json

---
+++ tcc/processing/converter.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+
+dir = './tcc/outputs/json/2021/08/'
+directory = os.listdir(dir)
+caminhos = [os.path.join(dir, name) for name in directory]
+arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
+jsons = [arq for arq in arquivos if arq.lower().endswith('.json')]
+
+count = 1
+result = []
+for path in jsons:
+    with open(path) as f:
+        data = json.load(f)
+        for entity in data.keys():
+            if entity == 'DIVERSOS':
+                for act in data[entity]:
+                    result.append({'id': count, 'text': act, 'label': []})
+                    count += 1
+            else:
+                titles = data[entity].keys()
+                for title in titles:
+                    for act in data[entity][title]:
+                        result.append({'id': count, 'text': act, 'label': []})
+                        count += 1
+
+json_object = json.dumps(result, indent=4)
+with open("dou_ac_08_21.json", "w") as outfile:
+    outfile.write(json_object)
